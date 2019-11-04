78d75ede0e390c3eca9cfe908f5c82efa6310083
dknoma <djknoma@gmail.com>
2019-11-04 00:45:39 -0800

    new enums

---
+++ src/main/java/kiyobot/util/BasicCommandType.java
@@ -0,0 +1,41 @@
+package kiyobot.util;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public enum BasicCommandType {
+    COMMANDS("!commands"),
+    HELP("!help"),
+    HEWWO("!hewwo"),
+    PING("!ping"),
+    DEFAULT();
+    
+    private static final Map<String, BasicCommandType> INSTANCE_BY_COMMAND = new HashMap<>();
+    
+    static {
+        Arrays.asList(BasicCommandType.values())
+                .forEach(instance -> INSTANCE_BY_COMMAND.put(instance.getCommand(), instance));
+    }
+    
+    private final String command;
+    
+    BasicCommandType() {
+        this.command = "";
+    }
+    
+    BasicCommandType(String command) {
+        this.command = command;
+    }
+    
+    public String getCommand() {
+        return command;
+    }
+    
+    // Pattern.compile("!ping").matcher(message).matches()
+    public static BasicCommandType getByCommandMessage(String message) {
+        final String[] parts = message.split("\\W++");
+        final String command = parts[0];
+        return INSTANCE_BY_COMMAND.getOrDefault(command, DEFAULT);
+    }
+}
--- src/main/java/kiyobot/util/CommandType.java
+++
@@ -1,4 +0,0 @@
-package kiyobot.util;
-
-public enum CommandType {
-}
